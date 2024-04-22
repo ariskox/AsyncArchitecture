68ba5d15dc2e402b8acf17d7555908a4a42987dd
Aris Koxaras <aris@koxaras.com>
2024-04-22 18:56:20 +0300

    Initial Commit

---
+++ AsyncArchitecture/AsyncArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncArchitectureApp.swift
+//  AsyncArchitecture
+//
+//  Created by Aris Koxaras on 22/4/24.
+//
+
+import SwiftUI
+
+@main
+struct AsyncArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
